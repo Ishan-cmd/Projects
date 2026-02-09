3a03cbefa8cb412e023f387d9040176106a9056a
Ishan Singh <86545965+Ishan-cmd@users.noreply.github.com>
2026-02-09 10:56:59 +0530

    Create Prediction_Response

---
+++ MyWork/FastAPI_ML_Model/schema/prediction_response.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+from typing import Dict
+
+class PredictionResponse(BaseModel):
+    predicted_category: str = Field(
+        ...,
+        description="The predicted insurance premium category",
+        example="High"
+    )
+    confidence: float = Field(
+        ...,
+        description="Model's confidence score for the predicted class (range: 0 to 1)",
+        example=0.8432
+    )
